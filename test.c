3de01c2e59562402350dce02d11532dcd2162ac7
waterlens <waterlens@waterlens.moe>
2025-01-18 20:21:15 +0800

    add split test

--- test.c
+++ test.c
@@ -7,4 +7,12 @@ int main() {
   assert(c.p == 771566984);
   assert(c.m == 514856152);
   assert(ek_m_crc32_finalize(c) == 3421780262);
+  const char s1[] = "12345";
+  const char s2[] = "6789";
+  ek_monoid_crc32_t c1 = ek_m_crc32_fold_bytes((void *)s1, sizeof(s1) - 1);
+  ek_monoid_crc32_t c2 = ek_m_crc32_fold_bytes((void *)s2, sizeof(s2) - 1);
+  ek_monoid_crc32_t c3 = ek_m_crc32_combine(c1, c2);
+  assert(c3.p == c.p);
+  assert(c3.m == c.m);
+  assert(ek_m_crc32_finalize(c3) == ek_m_crc32_finalize(c));
 }
